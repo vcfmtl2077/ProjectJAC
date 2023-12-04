1b8dc9b71f148ff1b843524d94d11cbd380f4726
weifeng <weifeng@weis-mac-studio.ht.home>
2023-12-04 11:35:04 -0500

    OOP assignment 1 project init

---
+++ src/oop/assignment/projecta/projectA.java
@@ -0,0 +1,10 @@
+package oop.assignment.projecta;
+
+public class projectA {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/oop/assignment/projectb/projectB.java
@@ -0,0 +1,10 @@
+package oop.assignment.projectb;
+
+public class projectB {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
