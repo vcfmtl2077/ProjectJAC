4f997e1346be6130841012723b7634ac29af2b0a
weifeng <weifeng@weis-mac-studio.ht.home>
2023-12-12 00:17:36 -0500

    OOP-Assignment3-game solution init

---
+++ src/oop/assignment3/LadderAndSnake.java
@@ -0,0 +1,24 @@
+// ----------------------------------------------------- 
+// Assignment 3)
+// Question: Ladder and Snake game/part 1
+// Written by: wei feng - 6284289
+// ----------------------------------------------------- 
+package oop.assignment3;
+
+import java.util.Random;
+
+public class LadderAndSnake {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+	private static int flipDice() {
+		Random dice = new Random();
+		return dice.nextInt(1, 6);
+	}
+	private static void play() {
+		
+	}
+}
---
+++ src/oop/assignment3/Player.java
@@ -0,0 +1,10 @@
+// ----------------------------------------------------- 
+// Assignment 3
+// Question: Ladder and Snake game/part1
+// Written by: wei feng - 6284289
+// ----------------------------------------------------- 
+package oop.assignment3;
+
+public class Player {
+
+}
