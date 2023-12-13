ad4800e66de2fbd25daa82794eeb8176d47666b3
weifeng <weifeng@weis-mac-studio.ht.home>
2023-12-13 10:27:26 -0500

    OOP-Assignment3-update part 2 solution.

--- src/oop/assignment3/LadderAndSnake.java
+++ src/oop/assignment3/LadderAndSnake.java
@@ -6,11 +6,12 @@
 package oop.assignment3;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class LadderAndSnake {
 
 	public static void main(String[] args) {
-		
+		Player[] playerArray = welcomeCheck();
 
 	}
 
@@ -20,5 +21,28 @@ public class LadderAndSnake {
 	}
 	private static void play() {
 		
+	}
+	private static Player[] welcomeCheck() {
+		System.out.println("********************************************");
+		System.out.println("*****  Welcome to Snake & Ladder game. *****");
+		System.out.println("********************************************");
+		System.out.println("Enter the number of players for your game - Number must be between 2 and 4 lunclusively:");
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		int retry = 1;
+		while(num<2||num>4) {
+			if(retry==4) {
+				System.out.println("Bad attempt "+retry+"! You have exhausted all your chances. Prohram will terminate!");
+				System.exit(0);
+			}
+			System.out.println("Bad attempt "+retry+" -Invalid # of players. Please enter a # between 2 and 4 inclusively:");
+			num = sc.nextInt();
+			retry++;
+		}
+		Player[] arr = new Player[num];
+		for(int i=0;i<arr.length;i++) {
+			arr[i] = new Player();
+		}
+		return arr;
 	}
 }
